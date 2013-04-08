9c998574a507a99ae64e1bfceca4ae47cefad5f2
Sasikala <Sasikala.V2@cognizant.com>
2013-04-08 18:49:08 +0530

    added assets configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ HtmlValidator::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  #config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -44,6 +45,7 @@ HtmlValidator::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( index.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
