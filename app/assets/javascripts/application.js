7e843b5a63fa66deaa41b307c0516ec5eb2de7af
Sasikala <Sasikala.V2@cognizant.com>
2013-04-18 16:37:29 +0530

    initial commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,4 +6,92 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+$(document).ready(function(){  
+ $("#validate_url").submit(function(e){
+    value=$("#uri").val()
+    if (value.length>0)
+    {
+    var url_check=0
+       $.each(value.split(","), function(index, arr_value) {
+	if((arr_value.indexOf("www.") > -1) && url_check==0)
+	{
+	url_check=0
+	}
+	else
+	{
+	url_check=1
+	}	
+	});
+	if (url_check==1)
+	{
+	url_check=0
+	 alert("Please enter valid Url");
+	e.preventDefault();
+	}
+	else
+	{
+		return true
+	}
+    }
+    else
+    {
+    alert("Please enter URL");
+     e.preventDefault();
+    }
+    });
+    
+    
+$('#files_attachment').change(function (e) {
+var input = document.getElementById("files_attachment");
+console.log(input)
+			var ul = document.getElementById("fileList");
+			while (ul.hasChildNodes()) {
+				ul.removeChild(ul.firstChild);
+			}
+			for (var i = 0; i < input.files.length; i++) {
+				var li = document.createElement("li");
+				li.innerHTML = input.files[i].name;
+				ul.appendChild(li);
+			}
+			if(!ul.hasChildNodes()) {
+				var li = document.createElement("li");
+				li.innerHTML = 'No Files Selected';
+				ul.appendChild(li);
+			}
+});
+
+$("#validate_file").submit(function(e){
+    var input_val = document.getElementById("files_attachment");
+    if (input_val.files.length>0)
+    {
+        var url_check=0
+    for (var i = 0; i < input_val.files.length; i++) {
+				 
+				 if((input_val.files[i].name.indexOf(".html") > -1) && url_check==0)
+				{
+				url_check=0
+				}
+				else
+				{
+				url_check=1
+				}	
+				
+			}
+			if (url_check==1)
+				{
+				url_check=0
+				 alert("Please upload html files");
+				e.preventDefault();
+				}
+				else
+				{
+					return true
+				}
+   }
+   else
+   {
+   alert("No file have been uploaded")
+   e.preventDefault();
+   }
+       });
+});
